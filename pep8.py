e169c9ecfea9fc9ee67aaee486fee27cf0997100
Emily Sena <emily.o.sena@hotmail.com>
2020-12-18 22:51:53 -0300

    ok

---
+++ pep8.py
@@ -0,0 +1,67 @@
+"""
+PEP 8 - Python Enhancement Proposal
+The Zen of Python
+import this
+1 - utilize Camel case para nomes de classes
+
+class Calculadoda:
+    pass
+
+
+class CalculadoraCientifica:
+    pass
+
+
+2 - utilize nomes em minusculo, separados por underlines para nomes de vairaveis e de funções
+
+def soma():
+    pass
+
+
+def soma_tres():
+    pass
+
+
+numero = 4
+
+numero_impar = 5
+
+
+3 - utilize 4 espaços para a identação(não use tab)
+
+
+if 'a' in 'banana':
+    print('tem')
+
+
+4 - linhas em branco
+- separar funções e classes com 2 linhas em branco
+- metodos dentro de uma classe separados por 2 linhas em branco
+
+5- Imports
+- Imports devem ser feitos sempre em linhas separadas
+import sys
+import os
+from types import StringType, ListType
+from types import(
+    StringType
+    etc
+)
+- imports devem ser feitos antes de tudo, logo depois dos comentários
+
+6 - espaços em expressóes e instruções
+Não faça:
+funcao( algo[ 1 ], { outro: 2 } )
+Faça:
+funcao(algo[1], {outro: 2}))
+7 - termine uma instrução sempre com uma nova linha
+
+#comentarios de uma linha só
+"""
+
+
+import this
+
+
+ a = (float)(input)
+
